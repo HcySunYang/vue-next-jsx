cb0284c270ce3f76f9c8e5f1ac2aba35132d1afd
HcySunYang <HcySunYang@outlook.com>
2020-07-01 22:31:30 +0800

    build: tweak build

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
 import { resolve } from 'path'
 import tsPlugin from 'rollup-plugin-typescript2'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'src/main.ts',
   output: {
     file: 'dist/main.js',
-    format: 'cjs',
+    format: 'cjs'
   },
   plugins: [
+    commonjs(),
     tsPlugin({
       tsconfig: resolve(__dirname, 'tsconfig.json'),
-      cacheRoot: resolve(__dirname, 'node_modules/.rts2_cache'),
-    }),
-  ],
+      cacheRoot: resolve(__dirname, 'node_modules/.rts2_cache')
+    })
+  ]
 }
